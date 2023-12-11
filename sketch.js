c7d31798096d42d446265cf68a1b0add2b6539b2
Jing Qian <jqian2272@gmail.com>
2023-12-11 08:02:10 -0500

    random image

--- sketch.js
+++ sketch.js
@@ -1,1 +1,30 @@
+class HeaderBar {
 
+}
+
+class Wind {
+
+}
+
+let img;
+function setup() {
+    createCanvas(800, 800);
+    img = getLoremImage();
+}
+
+function draw() {
+    image(img, 0, 0, img.width, img.height);
+}
+
+function getLoremImage(imgWidth, imgHeight, imgSubject) {
+    imgWidth = imgWidth ?? Math.floor(random() * 500) + 300;
+    imgHeight = imgHeight ?? Math.floor(random() * 500) + 300;
+    
+    if (typeof imgSubject !== undefined) {
+        let loadURL = `https://picsum.photos/${imgWidth}/${imgHeight}`;
+        console.log(loadURL);
+        return loadImage(loadURL);
+    } else {
+        return loadImage(`https://loremflickr.com/${imgWidth}/${imgHeight}/${imgSubject}`);
+    }
+}
