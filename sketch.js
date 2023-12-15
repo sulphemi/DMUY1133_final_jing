d7f0ab4da45fe15e788a4113b4b1ca3ddb0560af
Jing Qian <jqian2272@gmail.com>
2023-12-15 04:58:08 -0500

    landing page

--- sketch.js
+++ sketch.js
@@ -1,1 +1,46 @@
+let myInput;
+let myButton;
 
+function setup() {
+    createCanvas(800, 600);
+    background(50);
+
+    myInput = createInput();
+    myInput.position(300, 300);
+    myInput.size(200, 20);
+    myInput.style("font-family: monospace");
+
+    myButton = createButton("connect");
+    myButton.position(325, 350);
+    myButton.size(150, 20);
+    myButton.style("font-family: monospace");
+    myButton.style("background-color: #404040");
+    myButton.style("color: #ffffff");
+    myButton.style("border-radius: 5px");
+    myButton.attribute("onmouseover", "this.style.backgroundColor='#303030'");
+    myButton.attribute("onmouseout", "this.style.backgroundColor='#404040'");
+    myButton.mousePressed(connectToIP);
+}
+
+function validateIP(ip) {
+    let splitStr = ip.split(".");
+    if (splitStr.length != 4) return false;
+    
+    for (let x of splitStr) {
+        x = +x; //force type conversion into number
+        if (+x === NaN) return false; //x wasn't a number
+        if (x < 0 || x > 255) return false; //x not within acceptable bounds
+    }
+
+    return true; //passed all checks
+}
+
+function connectToIP() {
+    const addr = myInput.value();
+    if (validateIP(addr)) {
+        removeElements();
+        alert(`ip accepted. now connecting to ${addr}...`);
+    } else {
+        alert("not a valid ip!\nas a reminder, valid ip addresses follow the format x.x.x.x\nwhere x denotes a number in 0..255");
+    }
+}
