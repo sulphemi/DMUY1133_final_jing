8759f58b517b518268cab968f923b78adc3de3c9
Jing Qian <jqian2272@gmail.com>
2023-12-18 02:09:40 -0500

    manually merge from windoes

--- sketch.js
+++ sketch.js
@@ -42,6 +42,8 @@ class ImageFile {
         this.type = "image";
         this.name = name;
         this.cont = null;
+        this.de_x = RNG.nextInt(0, 400);
+        this.de_y = RNG.nextInt(0, 400);
     }
 
     populate() {
@@ -54,6 +56,8 @@ class TextFile {
         this.type = "text";
         this.name = name;
         this.cont = null;
+        this.de_x = RNG.nextInt(0, 400);
+        this.de_y = RNG.nextInt(0, 400);
     }
 }
 
@@ -75,6 +79,8 @@ class BinaryFile {
         this.name = name;
         this.cont = null;
         this.length = RNG.nextInt(16, 256);
+        this.de_x = RNG.nextInt(0, 400);
+        this.de_y = RNG.nextInt(0, 400);
 
         this.populate();
     }
@@ -183,6 +189,8 @@ function connectToIP() {
         //start
         createFileTree();
         alert(`ip accepted. now connecting to ${addr}...`);
+
+        startDE();
         //makeLoadingScreen();
     } else {
         alert("not a valid ip!\nas a reminder, valid ip addresses follow the format x.x.x.x\nwhere x denotes a number in 0..255");
@@ -209,4 +217,163 @@ function makeLoadingScreen() {
         };
     }
     loadingScreen = new p5(loadScreenCode, "meow");
+}
+
+
+
+///////////
+
+function startDE() {
+    headerColor = color(0, 0, 255);
+
+    draw = drawDE;
+}
+
+
+let windowSpawnX = 0;
+let windowSpawnY = 0;
+
+class HeaderBar {
+    constructor(content) {
+        this.content = content;
+        this.width = content.width;
+        this.height = 20;
+        this.pos = createVector(windowSpawnX += 20, windowSpawnY += 30);
+    }
+
+    mouseHovered() {
+        return (mouseX > this.pos.x) && (mouseX < this.pos.x + this.width) && (mouseY > this.pos.y) && (mouseY < this.pos.y + this.height);
+    }
+
+    closeHovered() {
+        return (mouseX > this.pos.x) && (mouseX < this.pos.x + 20) && (mouseY > this.pos.y) && (mouseY < this.pos.y + 20);
+    }
+
+    mouseOnContent() {
+        return (mouseX > this.pos.x) && (mouseX < this.pos.x + this.width) && (mouseY > this.pos.y) && (mouseY < this.pos.y + this.height + this.content.height);
+    }
+
+    display() {
+        fill(headerColor);
+        rect(this.pos.x, this.pos.y, this.width, this.height);
+        this.drawContent();
+        this.drawCloseButton();
+    }
+
+    drawCloseButton() {
+        fill(this.closeHovered ? color(255, 0, 0) : color(255, 50, 50));
+        square(this.pos.x, this.pos.y, 20);
+    }
+
+    drawContent() {
+        image(this.content, h.pos.x, h.pos.y + h.height, this.content.width, this.content.height);
+    }
+}
+
+class FileExplorer {
+    constructor() {
+        this.content = {};
+        this.content.width = 720;
+        this.content.height = 512;
+        this.pos = createVector(20, 20);
+    }
+
+    mouseHovered() {
+        return (mouseX > this.pos.x) && (mouseX < this.pos.x + this.width) && (mouseY > this.pos.y) && (mouseY < this.pos.y + this.height);
+    }
+
+    closeHovered() {
+        return (mouseX > this.pos.x) && (mouseX < this.pos.x + 20) && (mouseY > this.pos.y) && (mouseY < this.pos.y + 20);
+    }
+
+    mouseOnContent() {
+        return (mouseX > this.pos.x) && (mouseX < this.pos.x + this.width) && (mouseY > this.pos.y) && (mouseY < this.pos.y + this.height + this.content.height);
+    }
+
+    display() {
+        fill(headerColor);
+        rect(this.pos.x, this.pos.y, this.width, this.height);
+        this.drawContent();
+        this.drawCloseButton();
+    }
+
+    drawCloseButton() {
+        fill(this.closeHovered ? color(255, 0, 0) : color(255, 50, 50));
+        square(this.pos.x, this.pos.y, 20);
+    }
+
+    drawContent() {
+        rect(this.pos.x, this.pos.y, this.content.width, this.content.height);
+    }
+}
+
+let dragging = null;
+let headerColor;
+let headers = [];
+
+function drawDE() {
+    background(240);
+
+    for (f of fileTree.cont) {
+        rect(f.de_x, f.de_y, 30, 50);
+        text(f.name, f.de_x, f.de_y);
+    }
+
+    for (h of headers) {
+        h.display();
+    }
+}
+
+function getLoremImage(imgWidth, imgHeight, imgSubject) {
+    imgWidth = imgWidth ?? Math.floor(random() * 500) + 300;
+    imgHeight = imgHeight ?? Math.floor(random() * 500) + 300;
+
+    if (--loadLimit < 0) return {};
+    
+    if (typeof imgSubject === "undefined") {
+        //let loadURL = `https://picsum.photos/${imgWidth}/${imgHeight}`;
+        let loadURL = `https://source.unsplash.com/random/${imgWidth}x${imgHeight}`;
+        console.log(loadURL);
+        return loadImage(loadURL);
+    } else {
+        let loadURL = `https://loremflickr.com/${imgWidth}/${imgHeight}/${imgSubject}`;
+        console.log(loadURL);
+        return loadImage(loadURL);
+    }
+}
+
+function mousePressed() {
+    for (let i = headers.length - 1; i >= 0; i--) {
+        const h = headers[i];
+
+        if (h.closeHovered()) {
+            headers.splice(i, 1);
+            break;
+        }
+
+        if (h.mouseHovered()) {
+            headers.splice(i, 1);
+            headers.push(h);
+            dragging = h;
+            break;
+        }
+
+        if (h.mouseOnContent()) {
+            headers.splice(i, 1);
+            headers.push(h);
+            break;
+        }
+    }
+}
+
+function mouseReleased() {
+    dragging = null;
+}
+
+function mouseDragged(event) {
+    if (dragging !== null) {
+        e = event;
+        dragging.pos.x += event.movementX;
+        dragging.pos.y += event.movementY;
+    }
 }
